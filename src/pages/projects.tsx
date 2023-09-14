8b7f6b7ea0be5ce9c0ea062973c07095b12cd56c
camilatigre <milatigre1@gmail.com>
2023-09-14 17:32:22 -0300

    add header to projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,14 @@
 /* eslint-disable react/jsx-key */
 import React from 'react';
 
-const Projects = () => <>{'projects list'}</>;
+import Header from '../components/header';
+
+const Projects = () => (
+  <>
+    <Header />
+    <h1>Projects</h1>
+    {'projects list'}
+  </>
+);
 
 export default Projects;
